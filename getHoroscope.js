7db6f312412717454c1869951cb43ed6f710f6a3
Andrius <alimasWebDeveloper@gmail.com>
2021-03-06 17:35:09 +0000

    adding new file getHoroscope, where all logic will live, created couple variables arrays that will be helping to form our horocsope sentences

---
+++ getHoroscope.js
@@ -0,0 +1,8 @@
+// this variable is for result we will store all 3 sentences later on to form horoscope
+let result = "";
+
+// array of planets, where we can use with startPlanet arrays to make sentence
+let planet = ['mars','venus','jupiter','new moon','the sun','mercury'];
+
+// array of starting words with planets
+let startPlanet = ['Your sign is ','Wrong planet for you ','Better place ','Colliding planet ', 'Reach limit your sign ','Be aware this is '];
