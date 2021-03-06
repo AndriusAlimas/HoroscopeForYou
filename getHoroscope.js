1d3093e4ab42756d6b5e48ea9902cfccbcc92fdf
Andrius <alimasWebDeveloper@gmail.com>
2021-03-06 17:48:06 +0000

    Creating new function that will generate automatically from arrays and combine to single sentence

--- getHoroscope.js
+++ getHoroscope.js
@@ -6,3 +6,18 @@ let planet = ['mars','venus','jupiter','new moon','the sun','mercury'];
 
 // array of starting words with planets
 let startPlanet = ['Your sign is ','Wrong planet for you ','Better place ','Colliding planet ', 'Reach limit your sign ','Be aware this is '];
+
+// function that will accept two arguments beginning sentence and ending sentence ,
+// randomly get single string from arrays and returning combining those strings to full sentence
+const makeSentence = (beginning,ending) =>{
+    let result = "";
+    let index = Math.floor(Math.random() * beginning.length);
+    result += beginning[index];
+    index = Math.floor(Math.random() * ending.length);
+    result += ending[index];
+    return result;
+}
+
+// testing our method
+let firstSentence = makeSentence(startPlanet,planet);
+console.log(firstSentence);
